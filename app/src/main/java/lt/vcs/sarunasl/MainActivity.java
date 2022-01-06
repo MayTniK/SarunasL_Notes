6269d5af8334f6a7fd05cc25b7b54e1a96bf0ea2
MayTniKs <saraslasinskis@gmail.com>
2022-01-06 20:41:33 +0200

    Added ListView and ArrayAdapter

--- app/src/main/java/lt/vcs/sarunasl/MainActivity.java
+++ app/src/main/java/lt/vcs/sarunasl/MainActivity.java
@@ -1,9 +1,14 @@
 package lt.vcs.sarunasl;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,11 +17,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.textView);
-        TextView textView2 = findViewById(R.id.textView2);
 
-        textView.setText ("Some Text From Code");
-        textView.setTextSize(30.0f);
-        textView2.setText("Some second test from code");
+
+        List<String> notes = new ArrayList<>();
+        notes.add("1 - Name1 -Content1");
+        notes.add("2 - Name2 -Content2");
+        notes.add("3 - Name3 -Content3");
+        notes.add("4 - Name4 -Content4");
+        notes.add("5 - Name5 -Content5");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
+
+        ListView listView = findViewById(R.id.listView);
+
+        listView.setAdapter(arrayAdapter);
     }
 }
