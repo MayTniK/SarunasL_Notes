d00de4bb71ca1d7d24f261f668852382f3b3a1d7
MayTniKs <saraslasinskis@gmail.com>
2022-01-06 20:06:35 +0200

    All Code

--- app/src/main/java/lt/vcs/sarunasl/MainActivity.java
+++ app/src/main/java/lt/vcs/sarunasl/MainActivity.java
@@ -3,6 +3,7 @@ package lt.vcs.sarunasl;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        TextView textView2 = findViewById(R.id.textView2);
+
+        textView.setText ("Some Text From Code");
+        textView.setTextSize(30.0f);
+        textView2.setText("Some second test from code");
     }
 }
